d7a6536aedb4939a1c75413562a45ab77c73a0f4
SonTH <sonth@carpla.vn>
2023-07-07 17:06:19 +0700

    update notification

--- src/components/exam/ExamInfoHistory.jsx
+++ src/components/exam/ExamInfoHistory.jsx
@@ -17,6 +17,14 @@ function ExamInfoHistory({exam, item}) {
     const s = time % 60
     return `${m} phút ${s} giây`
   }
+
+  const aaaa = () => {
+    if (exam.subject === 9) {
+      return `Số câu đúng ${item.numberListeningQuestionRight} Listening - ${item.numberReadingQuestionRight} Reading`
+    } else {
+      return `Số câu đúng ${item.numberQuestionRight}/${exam.numberQuestion} câu`
+    }
+  }
   return (
     <div className={"p-2"}>
       <TitleExamItem className={"font-bold text-info flex items-center mb-2 cursor-pointer"}>
@@ -25,7 +33,7 @@ function ExamInfoHistory({exam, item}) {
       <div className={'bg-base-200 rounded-xl p-3 flex'}>
         <div className={'flex flex-col items-start space-y-2 flex-1'}>
           <CharacteristicsItem icon={question}>
-            Số câu đúng {item.numberQuestionRight}/{exam.numberQuestion} câu
+            {aaaa()}
           </CharacteristicsItem>
           <CharacteristicsItem icon={time}>
             Thời gian làm bài {timeSpentConvert(item.timeSpent)}
@@ -36,8 +44,9 @@ function ExamInfoHistory({exam, item}) {
         </div>
         <div>
           <div className="flex items-center text-primary flex-col border border-primary rounded-md">
-            <div className={'text-white bg-primary p-2 uppercase text-sm'}>Điểm số</div>
-            <div className="font-bold text-xl p-2">{(item.numberQuestionRight * 10 / exam.numberQuestion).toFixed(1)}</div>
+            <div
+              className={'text-white bg-primary p-2 uppercase text-sm'}>{exam.subject === 9 ? 'Kết quả' : 'Điểm số'} < /div>
+            <div className="font-bold text-xl p-2">{(item.point).toFixed(1)}</div>
           </div>
         </div>
       </div>
