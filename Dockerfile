ba6d931dce8f8f1b4b55ec48ecd8341b80245ad5
SonTH <sonth@carpla.vn>
2023-07-14 14:52:34 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ WORKDIR /app
 COPY . .
 RUN yarn
 
-#ENV REACT_APP_GOOGLE_CLIENT_ID="925232483768-fs45c8obk5mefufo6ob5tl20mdjrb35q.apps.googleusercontent.com"
-#ENV NEXT_PUBLIC_API_URL="https://api.trotot.online"
-#ENV NEXT_PUBLIC_COOKIE_ACCESS_TOKEN_KEY="cookie_access_token_key"
-#ENV NEXT_PUBLIC_COOKIE_REFRESH_ACCESS_TOKEN_KEY="cookie_refresh_access_token_key"
-#ENV NEXT_PUBLIC_SESSION_ID="cookie_session_id"
+ENV NEXT_PUBLIC_API_URL="139.180.212.5:8200/web"
+ENV NEXT_PUBLIC_COOKIE_ACCESS_TOKEN_KEY="cookie_access_token_key"
+ENV NEXT_PUBLIC_COOKIE_REFRESH_ACCESS_TOKEN_KEY="cookie_refresh_access_token_key"
 
 EXPOSE 3000
 RUN yarn build
--- src/components/home/SearchSection.jsx
+++ src/components/home/SearchSection.jsx
@@ -4,19 +4,26 @@ import Image from "next/image";
 import search from '@/assets/images/search-banner.jpg'
 
 function SearchSection(props) {
-    return (
-        <div className={'bg-white w-full'}>
-            <div className="container mx-auto padding-mobile grid grid-cols-2">
-                <div className={'w-full p-12 aspect-square'}>
-                    <Image src={search} alt={''} className={'object-cover w-full h-full'} layout={'fit'} />
-                </div>
-                <div className={'flex-1 my-auto px-4'}>
-                    <div className={'font-bold text-2xl'}>Ngân hàng đề thi trắc nghiệm online, tuyển tập bài giảng, bài học trực tuyến.</div>
-                    <FeatureSection/>
-                </div>
-            </div>
+  return (
+    <div className={'bg-white w-full'}>
+      <div className="container mx-auto padding-mobile grid-cols-2 md:grid hidden">
+        <div className={'w-full p-12 aspect-square'}>
+          <Image src={search} alt={''} className={'object-cover w-full h-full'} layout={'fit'}/>
         </div>
-    );
+        <div className={'flex-1 my-auto px-4'}>
+          <div className={'font-bold text-2xl'}>Ngân hàng đề thi trắc nghiệm online, tuyển tập bài giảng, bài học trực
+            tuyến.
+          </div>
+          <FeatureSection/>
+        </div>
+      </div>
+      <div className="container mx-auto padding-mobile md:hidden block">
+        <div className={'flex-1 my-auto px-4'}>
+          <FeatureSection/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchSection;
