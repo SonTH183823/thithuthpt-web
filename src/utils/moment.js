5ca3f7001f31aa1f49a34fd8c4638fb46c61af24
SonTH <sonth@carpla.vn>
2023-07-24 14:10:46 +0700

    update env

--- src/utils/moment.js
+++ src/utils/moment.js
@@ -1,8 +1,8 @@
 import moment from "moment";
 
 export const formatDate = (date) => {
-  return moment(date * 1000).utc().format("DD/MM/YYYY");
+  return moment(date * 1000).format("DD/MM/YYYY");
 };
 export const formatDateTime = (date) => {
-  return moment(date * 1000).utc().format("DD/MM/YYYY HH:mm ");
+  return moment(date * 1000).format("DD/MM/YYYY HH:mm ");
 };
